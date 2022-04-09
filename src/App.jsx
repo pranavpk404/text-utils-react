ed6c995ec12db5639f85843854dd653548aa4aa1
Pranav <pranavpk404@gmail.com>
2022-04-09 12:24:28 +0530

    some fixes

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import TextForm from "./components/TextForm";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <TextForm
+          heading="Try TextUtils - word counter, character counter, remove extra spaces"
+        />
+      </div>
+    </>
+  );
+}
+
+export default App;
